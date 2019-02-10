84a54938a0ea771a891c0cda0b8dc084d83ff1bd
Deji Kadri <dejikadri@gmail.com>
2019-02-10 19:52:58 +0100

    created API route and file writer

--- src/main/java/com/proto/app/UserService.java
+++ src/main/java/com/proto/app/UserService.java
@@ -1,13 +1,27 @@
 package com.proto.app;
 
+import java.io.PrintWriter;
+
 public class UserService {
 
-    //create file for write
 
     public String addUSer(User usr){
 
         //write user to file
         System.out.println(usr.getName());
+        String path = usr.getName()+".txt";
+
+        try {
+
+            PrintWriter writer = new PrintWriter(path, "UTF-8");
+            writer.print(usr.getName());
+            writer.close();
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+
+        }
+
         return "user "+ usr.getName()+" added!";
 
     }
